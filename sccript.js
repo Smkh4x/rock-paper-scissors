38568c06b3dafac54b8052117a9525cc576ea67e
Smk <159492095+Smkh4x@users.noreply.github.com>
2026-04-08 01:01:29 -0800

    update

--- sccript.js
+++ sccript.js
@@ -1,37 +1,26 @@
-
-// 1 hajara
-// 2 wara9a
-// 3 mi9ass
-
 let choice;
-
-
 let round = 0;
 let user = 0;
 let boot = 0;
 
-
 function main(){
+    
     let botRandom = Math.floor(Math.random() * 4);
+    
     if(botRandom === choice){
-       
         document.getElementById('result').innerHTML = "<h2>ta3adol</h2>"
-
     }else if(botRandom === 1 && choice === 2 || botRandom === 2 && choice === 3 || botRandom === 3 && choice === 1){
-       
+        
         user++, round++;
         document.getElementById('user-score').textContent = user
         document.getElementById('rounds').textContent = round
-            
+        
     } else {
-      
-        boot++
-        round++
+        
+        boot++, round++;
         document.getElementById('bot-score').textContent = boot
         document.getElementById('rounds').textContent = round
-    }
-
-    
+    }   
 }
 function rock(){
     choice = 1;
